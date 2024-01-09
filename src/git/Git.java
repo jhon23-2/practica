96c8d120108db1198c1f1e2e10726661c207cfc2
jhon23-2 <112707663+jhon23-2@users.noreply.github.com>
2024-01-09 16:17:44 -0500

    validacion de fechas

--- src/git/Git.java
+++ src/git/Git.java
@@ -23,6 +23,11 @@ public class Git {
         System.out.println("Dias: " + periodo.getDays());
         System.out.println("Meses: " + periodo.getMonths());
         System.out.println("Anios: " + periodo.getYears());
+        
+    }
+
+    public boolean validarFecha(LocalDate fechaAvalidar , LocalDate before , LocalDate after){
+        return !fechaAvalidar.isBefore(before) && !fechaAvalidar.isAfter(after);
     }
 
 }
