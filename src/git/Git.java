24296d69fa22053c55954e12f64ddc7755850f23
Jhonatan <jhoalmanza52@gmail.com>
2024-01-08 19:22:34 -0500

    Ajustes realizados

--- src/git/Git.java
+++ src/git/Git.java
@@ -1,10 +1,25 @@
 package git;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
 
 public class Git {
 
     public static void main(String[] args) {
-
+        // Formato de fechas en java
+        
+        LocalDate fecha = LocalDate.of(2003, Month.JANUARY, 24);
+        LocalDate ahora = LocalDate.now();
+        
+        Period periodo = Period.between(fecha, ahora);
+        
+        System.out.println("------------------------------------------");
+        
+        System.out.println("Dias: " + periodo.getDays());
+        System.out.println("Meses: " + periodo.getMonths());
+        System.out.println("Anios: " + periodo.getYears());
     }
 
 }
