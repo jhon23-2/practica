c3cff078d44e37d5dcc971eb828bc819e074dd58
Jhonatan <jhoalmanza52@gmail.com>
2024-01-10 21:49:34 -0500

    fechas a validar

--- src/git/Git.java
+++ src/git/Git.java
@@ -21,9 +21,15 @@ public class Git {
         System.out.println("Meses: " + periodo.getMonths());
         System.out.println("Anios: " + periodo.getYears());
         
+        LocalDate fechaAValirdar = LocalDate.of(2024, Month.JANUARY, 2);
+        LocalDate fechaAIncio = LocalDate.of(2024, Month.JANUARY, 1);
+        LocalDate fechaFin = LocalDate.of(2024, Month.JANUARY, 10);
+        
+        System.out.println("La fecha es: " + validarFecha(fechaAValirdar, fechaAIncio, fechaFin) );
+        
     }
 
-    public boolean validarFecha(LocalDate fechaAvalidar , LocalDate before , LocalDate after){
+    public static boolean validarFecha(LocalDate fechaAvalidar , LocalDate before , LocalDate after){
         return !fechaAvalidar.isBefore(before) && !fechaAvalidar.isAfter(after);
     }
 
