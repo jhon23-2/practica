3a9ee9932725932ab20a0f87c1f2962ecf20c1fe
Jhonatan <jhoalmanza52@gmail.com>
2024-01-10 21:54:38 -0500

    cambios registrados desde la rama datos

--- src/git/Git.java
+++ src/git/Git.java
@@ -3,33 +3,34 @@ package git;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
-
+import java.util.Scanner;
 
 public class Git {
 
     public static void main(String[] args) {
         // Formato de fechas en java
-        
+        Scanner leer = new Scanner(System.in);
+
         LocalDate fecha = LocalDate.of(2003, Month.JANUARY, 24);
-        LocalDate ahora = LocalDate.now(); 
-        
+        LocalDate ahora = LocalDate.now();
+
         Period periodo = Period.between(fecha, ahora);
-        
+
         System.out.println("------------------------------------------");
-        
+
         System.out.println("Dias: " + periodo.getDays());
         System.out.println("Meses: " + periodo.getMonths());
         System.out.println("Anios: " + periodo.getYears());
-        
+
         LocalDate fechaAValirdar = LocalDate.of(2024, Month.JANUARY, 2);
         LocalDate fechaAIncio = LocalDate.of(2024, Month.JANUARY, 1);
         LocalDate fechaFin = LocalDate.of(2024, Month.JANUARY, 10);
-        
-        System.out.println("La fecha es: " + validarFecha(fechaAValirdar, fechaAIncio, fechaFin) );
-        
+
+        System.out.println("La fecha es: " + validarFecha(fechaAValirdar, fechaAIncio, fechaFin));
+
     }
 
-    public static boolean validarFecha(LocalDate fechaAvalidar , LocalDate before , LocalDate after){
+    public static boolean validarFecha(LocalDate fechaAvalidar, LocalDate before, LocalDate after) {
         return !fechaAvalidar.isBefore(before) && !fechaAvalidar.isAfter(after);
     }
 
